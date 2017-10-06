7127f02056e4a6ea20ebdf676d4cb2cd625e060c
Nathaniel Lovin <lovinn@carleton.edu>
2017-10-06 00:56:35 -0500

    added empty linkedlist.c

---
+++ linkedlist.c
@@ -0,0 +1,33 @@
+/* LinkedList
+Right now, just function declarations with no code so we can build it.
+*/
+
+#include <stdbool.h>
+#include "value.h"
+
+Value *makeNull() {
+}
+
+Value *cons(Value *car, Value *cdr) {
+}
+
+void display(Value *list) {
+}
+
+Value *car(Value *list) {
+}
+
+Value *cdr(Value *list) {
+}
+
+bool isNull(Value *value) {
+}
+
+int length(Value *value) {
+}
+
+Value *reverse(Value *list) {
+}
+
+void cleanup(Value *list) {
+}
