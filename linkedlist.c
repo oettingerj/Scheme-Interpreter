0f6e1bf8a4ec6e0fd5d69414279a410e010fb016
Nathaniel Lovin <lovinn@carleton.edu>
2017-10-12 19:39:14 -0500

    cdr and car

--- linkedlist.c
+++ linkedlist.c
@@ -4,6 +4,7 @@ Right now, just function declarations with no code so we can build it.
 
 #include <stdbool.h>
 #include "value.h"
+#include <assert.h>
 
 Value *makeNull() {
 }
@@ -15,9 +16,14 @@ void display(Value *list) {
 }
 
 Value *car(Value *list) {
+	assert((*list).type == CONS_TYPE);
+	return (*list).c.car;
 }
 
 Value *cdr(Value *list) {
+	assert((*list).type == CONS_TYPE);
+        return (*list).c.cdr;
+
 }
 
 bool isNull(Value *value) {
