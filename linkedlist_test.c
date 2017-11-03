1a7be2b9f383827a3101a8ab7e40233f5f5b34a1
Josh Oettinger <oettingerj@carleton.edu>
2017-11-02 21:47:22 -0500

    Made to use talloc

--- linkedlist_test.c
+++ linkedlist_test.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include "value.h"
 #include "linkedlist.h"
+#include "talloc.c"
 
 int main() {
-   Value *val1 = malloc(sizeof(Value));
+   Value *val1 = talloc(sizeof(Value));
    val1->type = INT_TYPE;
    val1->i = 12;
 
-   Value *val2 = malloc(sizeof(Value));
+   Value *val2 = talloc(sizeof(Value));
    val2->type = DOUBLE_TYPE;
    val2->d = 4.3;
 
@@ -19,5 +20,5 @@ int main() {
    display(head);
    printf("Length = %i\n", length(head));
    printf("Empty? %i\n", isNull(head));
-   cleanup(head);
+   texit(1);
 }
